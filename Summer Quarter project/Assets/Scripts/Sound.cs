2af64010a8e7e8acecca84ff4f3bbf52f09ee473
JosephShaw <Joseph.shaw@hgs.hiddengeniusproject.org>
2026-06-29 15:12:49 -0700

    Audio
    Audio done

---
+++ Summer Quarter project/Assets/Scripts/Sound.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+    public AudioClip audioClip;
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(0.1f, 2f)]
+    public float pitch = 1f;
+    public bool loop = false;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+
+}
