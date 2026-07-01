51a6a8ec662dfdaa35a38a887cf1158552ea5bb3
JosephShaw <Joseph.shaw@hgs.hiddengeniusproject.org>
2026-07-01 15:06:29 -0700

    goal
    made my goal say GOOAL when player runs into it

--- Summer Quarter project/Assets/Scripts/Enemy.cs
+++ Summer Quarter project/Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Transform target;
@@ -24,6 +25,48 @@ public class Enemy : MonoBehaviour
         if (player != null)
         {
             Debug.Log($"{gameObject.name} hit {collision.gameObject.name}!");
+        }
+    }
+}
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Transform target;
+    public Text loseText;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (loseText != null)
+        {
+            loseText.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER LOST");
+
+            if (loseText != null)
+            {
+                loseText.gameObject.SetActive(true);
+                loseText.text = "LOSE";
+            }
+
+            agent.isStopped = true;
         }
     }
 }
--- Summer Quarter project/Assets/Scripts/goal.cs
+++ Summer Quarter project/Assets/Scripts/goal.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using TMPro; // Include the TextMesh Pro namespace
 
 public class goal : MonoBehaviour
 {
     // Private variable of type int to track how many poitns the goal give
-    private int pointValue = 1;   
+    private int PointValue = 1;   
 
     // this function is called any time another collider enters this goal's collider trigger
     private void OnTriggerEnter(Collider other)
@@ -24,3 +25,33 @@ public class goal : MonoBehaviour
     }
 }
 
+
+public class Goal : MonoBehaviour
+{
+    // How many points the goal is worth
+    private int PointValue = 1;
+
+    // Drag your TextMeshPro object here in the Inspector
+    public TMP_Text goalText;
+
+    void Start()
+    {
+        // Hide the text when the game starts
+        goalText.gameObject.SetActive(false);
+    }
+
+    // Called when something enters the goal
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            Debug.Log("We Scored!");
+
+            // Show the text
+            goalText.gameObject.SetActive(true);
+            goalText.text = "GOOOOOAL!";
+        }
+    }
+}
